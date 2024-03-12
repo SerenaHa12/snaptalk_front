f3a6da7775e0e943dd7b6b34c867441ddd295581
SerenaHa12 <serenavander.ha@gmail.com>
2024-03-12 22:22:05 +0700

    continue: function create server by button add server

--- components/template/navigations/navigation-action.tsx
+++ components/template/navigations/navigation-action.tsx
@@ -6,9 +6,9 @@ const NavigationAction = () => {
     <div>
       <ActionTooltip side="right" align="center" label="Add server">
         <button className="group">
-          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-indigo-700">
+          <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-blue-700">
             <Plus
-              className="group-hover:text-white transition text-indigo-700"
+              className="group-hover:text-white transition text-blue-700"
               size={25}
             />
           </div>
